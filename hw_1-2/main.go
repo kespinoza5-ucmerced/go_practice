4aeaafcf018778817d96153de7137324a263d182
Kevin Espinoza <kespinoza5@ucmerced.edu>
2023-02-15 22:35:29 -0800

    Implemented constructor, Add_course, gpa All functions are implemented and commented.
    This makes problem 1, task 1 complete

--- hw_1-2/main.go
+++ hw_1-2/main.go
@@ -7,7 +7,18 @@ import (
 )
 
 func main() {
-	s := student.Student{}
+	name := "Kevin"
+	id := "100741789"
 
-	fmt.Println(s)
+	s := student.NewStudent(name, id)
+
+	fmt.Println(*s)
+
+	s.Add_course("CSCI780", 16, 3.70)
+	s.Add_course("CSE177", 16, 2.87)
+	s.Add_course("CSE195", 16, 4.0)
+	s.Add_course("ENGR191", 4, 4.0)
+	s.Add_course("WRI105", 16, 3.52)
+
+	fmt.Printf("This student's GPA is: %f\n", s.Calculate_gpa())
 }
--- student/student.go
+++ student/student.go
@@ -9,5 +9,35 @@ type course struct {
 type Student struct {
 	name, id, major string
 	age             int64
-	courses         course
+	courses         []course
+}
+
+// NewStudent initializes Student with given name and id
+func NewStudent(_name, _id string) *Student {
+	return &Student{
+		name: _name,
+		id:   _id,
+	}
+}
+
+// Add_course takes args and pushes into that student's course list
+func (s *Student) Add_course(course_name string, credit_hours int64, grade float64) {
+	s.courses = append(s.courses, course{course_name, credit_hours, grade})
+}
+
+// Calculate_gpa returns gpa for student across all courses in their course list
+// GPA = sum(grade * credit_hours) / sum(credit_hours)
+func (s *Student) Calculate_gpa() float64 {
+	if len(s.courses) == 0 {
+		return 0
+	}
+
+	var credit_sum, grade_credit_sum float64
+
+	for _, course := range s.courses {
+		credit_sum += float64(course.hours)
+		grade_credit_sum += course.grade * float64(course.hours)
+	}
+
+	return grade_credit_sum / credit_sum
 }
