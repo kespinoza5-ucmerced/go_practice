ea92bcd5d5c2bc8e9353a7d147ac2262bfcaff49
Kevin Espinoza <kespinoza5@ucmerced.edu>
2023-02-15 23:46:12 -0800

    Defined all functions in database.go All functions are defined and commented.
    Main has been modified to have functions to test each package individually.

--- database/database.go
+++ database/database.go
@@ -7,27 +7,57 @@ type Database struct {
 }
 
 // Add_student adds a student to database and returns a pointer to it
-func (d *Database) Add_student(name, id string) *student.Student {
-	return &student.Student{}
+func (d *Database) Add_student(_name, _id string) *student.Student {
+	d.Students = append(d.Students, *student.NewStudent(_name, _id))
+	new_student_index := len(d.Students) - 1
+
+	return &d.Students[new_student_index]
 }
 
 // Find_student_by_id returns object with matching student id, otherwise returns nil
-func (d *Database) Find_student_by_id(id string) student.Student {
-	return student.Student{}
+func (d *Database) Find_student_by_id(_id string) *student.Student {
+	for _, student := range d.Students {
+		if student.Id == _id {
+			return &student
+		}
+	}
+
+	return nil
 }
 
 // Find_student_by_name returns object with matching student name, otherwise returns nil
+//
 // Assumes student's name is unique
-func (d *Database) Find_student_by_name(id string) student.Student {
-	return student.Student{}
+func (d *Database) Find_student_by_name(_name string) *student.Student {
+	for _, student := range d.Students {
+		if student.Name == _name {
+			return &student
+		}
+	}
+
+	return nil
 }
 
-// Find_student_by_id returns list of students taking the course, otherwise returns nil
-func (d *Database) Find_students_by_course(course_name string) []student.Student {
-	return []student.Student{}
+// Find_student_by_course returns list of students taking the course, otherwise returns nil
+func (d *Database) Find_students_by_course(_course_name string) []student.Student {
+	var students_taking_course []student.Student
+
+	for _, student := range d.Students {
+		for _, course := range student.Courses {
+			if course.Name == _course_name {
+				students_taking_course = append(students_taking_course, student)
+			}
+		}
+	}
+
+	if len(students_taking_course) == 0 {
+		return nil
+	}
+
+	return students_taking_course
 }
 
-// Num_student
+// Num_student returns number of students in database
 func (d *Database) Num_student() int {
-	return 0
+	return len(d.Students)
 }
--- hw_1-2/main.go
+++ hw_1-2/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"fmt"
 
+	// "github.com/kespinoza5-ucmerced/go_practice/database"
 	"github.com/kespinoza5-ucmerced/go_practice/database"
 	"github.com/kespinoza5-ucmerced/go_practice/student"
 )
 
-func main() {
-	name := "Kevin"
-	id := "100741789"
-
+func test_student(name, id string) {
 	s := student.NewStudent(name, id)
 
 	fmt.Println(*s)
@@ -22,17 +20,52 @@ func main() {
 	s.Add_course("WRI105", 16, 3.52)
 
 	fmt.Printf("This student's GPA is: %f\n", s.Calculate_gpa())
+}
 
+func test_database(name, id string) {
 	db := database.Database{}
 	db.Add_student(name, id)
 
 	fmt.Println(db)
 
-	// db.Students[0].Add_course("CSCI780", 16, 3.70)
-	// db.Students[0].Add_course("CSE177", 16, 2.87)
-	// db.Students[0].Add_course("CSE195", 16, 4.0)
-	// db.Students[0].Add_course("ENGR191", 4, 4.0)
-	// db.Students[0].Add_course("WRI105", 16, 3.52)
+	db.Students[0].Add_course("CSCI780", 16, 3.70)
+	db.Students[0].Add_course("CSE177", 16, 2.87)
+	db.Students[0].Add_course("CSE195", 16, 4.0)
+	db.Students[0].Add_course("ENGR191", 4, 4.0)
+	db.Students[0].Add_course("WRI105", 16, 3.52)
 
-	// fmt.Printf("This student's GPA is: %f\n", db.Students[0].Calculate_gpa())
+	// fmt.Println(db)
+	// fmt.Println(db.Students[0])
+	fmt.Printf("This student's GPA is: %f\n", db.Students[0].Calculate_gpa())
+
+	fmt.Println(db.Find_student_by_id("100741789"))
+	fmt.Println(db.Find_student_by_name("Kevin"))
+
+	db.Add_student("Rocky", "100629812")
+	db.Add_student("Biggie", "100331265")
+
+	db.Students[1].Add_course("CSE177", 16, 3.40)
+	db.Students[1].Add_course("CSE176", 16, 3.69)
+	db.Students[2].Add_course("ENGR191", 16, 4.0)
+	db.Students[2].Add_course("CSE176", 16, 3.92)
+
+	fmt.Println()
+	fmt.Println("Students taking CSE177: ")
+	fmt.Println(db.Find_students_by_course("CSE177"))
+
+	fmt.Println()
+	fmt.Println("Students taking CSE176: ")
+	fmt.Println(db.Find_students_by_course("CSE176"))
+
+	fmt.Println()
+	fmt.Printf("Num students in db: %d\n", db.Num_student())
+}
+
+func main() {
+	name := "Kevin"
+	id := "100741789"
+
+	// test_student(name, id)
+	test_database(name, id)
+
 }
