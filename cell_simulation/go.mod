1e44ec868fddb01035c2281bd389927f6f201846
Kevin Espinoza <kespinoza5@ucmerced.edu>
2023-02-19 01:19:44 -0800

    Added files for cell_simulation File is setup mostly according to tutorial, but having issue with CellSplitEvent not being recognized as a type and h.engine.Schedule(event) and sim.NewEngine() showing up as undefined

---
+++ cell_simulation/go.mod
@@ -0,0 +1,8 @@
+module github.com/kespinoza5-ucmerced/go_practice/cell_simulation
+
+go 1.20
+
+require (
+	github.com/rs/xid v1.4.0 // indirect
+	gitlab.com/akita/akita/v3 v3.0.0-alpha.21 // indirect
+)
---
+++ cell_simulation/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+
+	"gitlab.com/akita/akita/v3/sim"
+)
+
+type CellSplitEvent struct {
+	time    sim.VTimeInSec
+	handler sim.Handler
+}
+
+func (e CellSplitEvent) Time() sim.VTimeInSec {
+	return e.time
+}
+
+// Returns the handler that can should handle the event
+func (e CellSplitEvent) Handler() sim.Handler {
+	return e.handler
+}
+
+// IsSecondary tells if the event is a secondary event. Secondary event are
+// handled after all same-time primary events are handled.
+func (e CellSplitEvent) IsSecondary() bool {
+	return false
+}
+
+type CellSplitEventHandler struct {
+	cellCount int
+	engine    *sim.Engine
+}
+
+func (h *CellSplitEventHandler) Handle(e sim.Event) error {
+	// check type of event
+	CellSplitEvent, ok := e.(CellSplitEvent)
+	if !ok {
+		panic("wrong event type")
+	}
+
+	// perform split
+	h.cellCount++
+	fmt.Printf("Cell split at time %f, cell count: %d\n",
+		CellSplitEvent.time, h.cellCount)
+
+	// randomly generate number between 1-2
+	timeUntilNextSplit := sim.VTimeInSec(rand.Float64() + 1)
+
+	// create an event that adds current time and timeUntilNextSplit
+	event := CellSplitEvent{
+		time:    CellSplitEvent.time + timeUntilNextSplit,
+		handler: h,
+	}
+
+	// stop simulation at 200s
+	if CellSplitEvent.time < 200 {
+		h.engine.Schedule(event)
+	}
+
+	return nil
+}
+
+func main() {
+	engine := sim.NewEngine()
+	handler := &CellSplitEventHandler{
+		engine: engine,
+	}
+	event := CellSplitEvent{
+		time:    0,
+		handler: handler,
+	}
+	engine.Schedule(event)
+
+	engine.Run()
+}
